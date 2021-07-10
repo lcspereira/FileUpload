27676dd0d6a029ff834cf2d22dd306be6e43059d
Lucas Pereira <lucas.pereira6c@gmail.com>
2021-07-10 16:44:01 -0300

    Implemented File model

---
+++ database/migrations/2021_07_08_201207_create_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->integer('size');
+            $table->string('path');
+            $table->timestamps();
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
