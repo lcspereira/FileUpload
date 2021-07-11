8709acf2403fa60ccb07d4819d07e7eb686fdcd8
Lucas Pereira <lucas.pereira6c@gmail.com>
2021-07-11 00:00:03 -0300

    Created file controller

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Services\FileService;
+
+class FileController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+        return view('files',compact('files'));
+    }
+
+    public function listFiles()
+    {
+        try {
+            $user = Auth::user();
+            $fs = new FileService($user);
+            $files = $fs->listFilesForUser();
+            return response()->json($files);
+        } catch (\Exception $e) {
+            return response($e->getMessage(), 500)
+                        ->header('Content-type', 'text/plain');
+        }
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $user = Auth::user();
+            $fs = new FileService($user);
+            $msg = "File created successfully.";
+                $validator = $request->validate([
+                    'file' => 'required|max:8388608'
+                ]);
+            $fs->saveFile($request->file());
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        } finally {
+            return redirect('/files')->with('message', $msg);
+        }
+    }
+
+    public function delete(int $idFile)
+    {
+        try {
+            $user = Auth::user();
+            $fs = new FileService($user);
+            $status = [
+                'status' => 0,
+                'message' => 'File was deleted successfully.'
+            ];
+            $fs->deleteFile($idFile);
+        } catch (\Exception $e) {
+            $status = [
+                'status' => -1,
+                'message' => 'Error deleting file: ' . $e->getMessage()
+            ];
+        } finally {
+            return response()->json($status);
+        }
+    }
+
+    public function rename (Request $request, int $idFile, string $newName)
+    {
+        try {
+            $user = Auth::user();
+            $fs = new FileService($user);
+            $status = [
+                'status' => 0,
+                'message' => 'File was renamed successfully.'
+            ];
+            $fs->renameFile($idFile, $newName);
+        } catch (\Exception $e) {
+            $status = [
+                'status' => -2,
+                'message' => 'Error renaminc file: ' . $e->getMessage()
+            ];
+        } finally {
+            return response()->json($status);
+        }
+    }
+
+    public function download (int $idFile)
+    {
+        $user = Auth::user();
+        $fs = new FileService($user);
+        return $fs->downloadFile($idFile);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\File;
+use App\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -23,7 +24,12 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/files', function () {
-    $files = File::all();
-    return view('files',compact('files'));
-})->name('files');
+Route::middleware(['auth:sanctum', 'verified'])->get('/files', [FileController::class, 'index'])->name('files');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/files/create', [FileController::class, 'create'])->name('files.create');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/files/rename/{idFile}', [FileController::class, 'rename'])->name('files.rename');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/files/delete/{idFile}', [FileController::class, 'delete'])->name('files.delete');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/files/download/{idFile}', [FileController::class, 'download'])->name('files.download');
